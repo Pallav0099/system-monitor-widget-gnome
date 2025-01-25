73c3a4cc8f4a00a22010defaeb4ddb5400d2bd42
Pallav0099 <pallavsworkspace@gmail.com>
2025-01-25 12:13:31 +0530

    using a generic function for all the graphs, also added disk usage

--- metrics.py
+++ metrics.py
@@ -8,3 +8,15 @@ def get_memory_usage():
     """Returns the current memory usage percentage."""
     return psutil.virtual_memory().percent
 
+def get_disk_usage():
+    """Returns the current memory usage percentage."""
+    return psutil.disk_usage('/').percent
+
+def get_net_connections():
+    psutil.net_connections(kind='all')
+
+def get_sensors_temperatures():
+    return psutil.sensors_temperatures(fahrenheit=False)
+
+def get_sensors_fans():
+    return psutil.sensors_fans()
--- system_monitor_widget.py
+++ system_monitor_widget.py
@@ -1,16 +1,20 @@
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk, GLib
-from metrics import get_cpu_usage, get_memory_usage
+from metrics import get_cpu_usage, get_memory_usage, get_disk_usage
 
 class SystemMonitorWindow(Gtk.Window):
     def __init__(self):
         super().__init__(title="System Monitor")
         self.set_default_size(300, 400)
 
-        # Get screen dimensions
-        screen_width = Gdk.Screen.get_default().get_width()
-        screen_height = Gdk.Screen.get_default().get_height()
+        # Get screen dimensions using Gdk.Display and Gdk.Monitor
+        display = Gdk.Display.get_default()
+        monitor = display.get_primary_monitor()
+        geometry = monitor.get_geometry()
+        screen_width = geometry.width
+        screen_height = geometry.height
+
         window_width, window_height = self.get_default_size()
         self.move(0, (screen_height - window_height) // 2)
 
@@ -20,6 +24,7 @@ class SystemMonitorWindow(Gtk.Window):
         # Initialize data for graphs
         self.cpu_usage = []
         self.memory_usage = []
+        self.disk_usage = []
 
         # Create a Box to organize the content vertically
         self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
@@ -27,7 +32,7 @@ class SystemMonitorWindow(Gtk.Window):
 
         # CPU Graph Section
         self.cpu_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=5)
-        self.cpu_label = Gtk.Label(label="CPU")
+        self.cpu_label = Gtk.Label(label="CPU Usage")
         self.drawing_area_cpu = Gtk.DrawingArea()
         self.drawing_area_cpu.set_name("cpu-drawing-area")
         self.drawing_area_cpu.connect("draw", self.on_draw_cpu)
@@ -36,26 +41,26 @@ class SystemMonitorWindow(Gtk.Window):
 
         # Memory Graph Section
         self.memory_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=5)
-        self.memory_label = Gtk.Label(label="Memory")
+        self.memory_label = Gtk.Label(label="Memory Usage")
         self.drawing_area_memory = Gtk.DrawingArea()
         self.drawing_area_memory.set_name("memory-drawing-area")
         self.drawing_area_memory.connect("draw", self.on_draw_memory)
         self.memory_box.pack_start(self.memory_label, False, False, 0)
         self.memory_box.pack_start(self.drawing_area_memory, True, True, 0)
 
-        # Add both sections to the main Box
+        # Disk Usage Section
+        self.disk_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=5)
+        self.disk_label = Gtk.Label(label="Disk Usage")
+        self.drawing_area_disk = Gtk.DrawingArea()
+        self.drawing_area_disk.set_name("disk-drawing-area")
+        self.drawing_area_disk.connect("draw", self.on_draw_disk)
+        self.disk_box.pack_start(self.disk_label, False, False, 0)
+        self.disk_box.pack_start(self.drawing_area_disk, True, True, 0)
+
+        # Add all sections to the main Box
         self.box.pack_start(self.cpu_box, True, True, 0)
         self.box.pack_start(self.memory_box, True, True, 0)
-
-        # Set up drag handling for repositioning
-        self.connect("button-press-event", self.on_button_press)
-        self.connect("motion-notify-event", self.on_mouse_move)
-        self.connect("button-release-event", self.on_button_release)
-
-        # Variables for dragging
-        self.dragging = False
-        self.prev_x = 0
-        self.prev_y = 0
+        self.box.pack_start(self.disk_box, True, True, 0)
 
         # Start a timer to update the graph every second
         GLib.timeout_add(1000, self.update_data)
@@ -64,7 +69,8 @@ class SystemMonitorWindow(Gtk.Window):
         """Load and apply CSS styles from the styles.css file."""
         css_provider = Gtk.CssProvider()
         css_provider.load_from_path("styles.css")
-        screen = Gdk.Screen.get_default()
+        display = Gdk.Display.get_default()
+        screen = display.get_default_screen()
         style_context = Gtk.StyleContext()
         style_context.add_provider_for_screen(
             screen,
@@ -73,78 +79,61 @@ class SystemMonitorWindow(Gtk.Window):
         )
 
     def update_data(self):
-        """Fetch new data for CPU and memory usage and update the graph."""
-        self.cpu_usage.append(get_cpu_usage())
-        self.memory_usage.append(get_memory_usage())
+        """Fetch new data for CPU, memory, and disk usage."""
+        cpu = get_cpu_usage()
+        memory = get_memory_usage()
+        disk = get_disk_usage()
+
+        self.cpu_usage.append(cpu)
+        self.memory_usage.append(memory)
+        self.disk_usage.append(disk)
 
         if len(self.cpu_usage) > 50:
             self.cpu_usage.pop(0)
         if len(self.memory_usage) > 50:
             self.memory_usage.pop(0)
+        if len(self.disk_usage) > 50:
+            self.disk_usage.pop(0)
 
         self.drawing_area_cpu.queue_draw()  # Trigger redraw for CPU graph
         self.drawing_area_memory.queue_draw()  # Trigger redraw for Memory graph
+        self.drawing_area_disk.queue_draw()  # Trigger redraw for Disk graph
+
         return True
 
     def on_draw_cpu(self, widget, cr):
-        """Draw the CPU usage graph with a rectangle around it."""
-        width, height = widget.get_allocated_width(), widget.get_allocated_height()
-        cr.set_source_rgb(1, 1, 1)  # White background
-        cr.paint()
-
-        # Draw a rectangle around the CPU graph
-        cr.set_source_rgb(0, 0, 0)  # Black border
-        cr.set_line_width(2)
-        cr.rectangle(0, 0, width, height)
-        cr.stroke()
-
-        # Draw CPU usage graph (Blue)
-        cr.set_source_rgb(0, 0, 1)
-        for i in range(1, len(self.cpu_usage)):
-            cr.move_to(i * (width / 50), height - (self.cpu_usage[i-1] / 100 * height))
-            cr.line_to((i+1) * (width / 50), height - (self.cpu_usage[i] / 100 * height))
-            cr.stroke()
+        """Draw the CPU usage graph."""
+        self.draw_graph(widget, cr, self.cpu_usage, (0, 0, 1))  # Blue for CPU
 
     def on_draw_memory(self, widget, cr):
-        """Draw the Memory usage graph with a rectangle around it."""
+        """Draw the Memory usage graph."""
+        self.draw_graph(widget, cr, self.memory_usage, (0, 1, 0))  # Green for Memory
+
+    def on_draw_disk(self, widget, cr):
+        """Draw the Disk usage graph."""
+        self.draw_graph(widget, cr, self.disk_usage, (1, 0, 0))  # Red for Disk
+
+    def draw_graph(self, widget, cr, data, color):
+        """Generic function to draw a graph."""
         width, height = widget.get_allocated_width(), widget.get_allocated_height()
         cr.set_source_rgb(1, 1, 1)  # White background
         cr.paint()
 
-        # Draw a rectangle around the Memory graph
+        # Draw the graph border
         cr.set_source_rgb(0, 0, 0)  # Black border
         cr.set_line_width(2)
         cr.rectangle(0, 0, width, height)
         cr.stroke()
 
-        # Draw Memory usage graph (Green)
-        cr.set_source_rgb(0, 1, 0)
-        for i in range(1, len(self.memory_usage)):
-            cr.move_to(i * (width / 50), height - (self.memory_usage[i-1] / 100 * height))
-            cr.line_to((i+1) * (width / 50), height - (self.memory_usage[i] / 100 * height))
+        # Draw the graph data
+        cr.set_source_rgb(*color)
+        for i in range(1, len(data)):
+            cr.move_to((i - 1) * (width / 50), height - (data[i - 1] / 100 * height))
+            cr.line_to(i * (width / 50), height - (data[i] / 100 * height))
             cr.stroke()
-
-    def on_button_press(self, widget, event):
-        """Start dragging the window."""
-        self.dragging = True
-        self.prev_x, self.prev_y = event.x, event.y
-
-    def on_mouse_move(self, widget, event):
-        """Handle window dragging while the mouse moves."""
-        if self.dragging:
-            dx = event.x - self.prev_x
-            dy = event.y - self.prev_y
-            x, y = self.get_position()
-            self.move(x + dx, y + dy)
-            self.prev_x, self.prev_y = event.x, event.y
-
-    def on_button_release(self, widget, event):
-        """Stop dragging the window."""
-        self.dragging = False
 
 if __name__ == "__main__":
     win = SystemMonitorWindow()
     win.connect("destroy", Gtk.main_quit)
     win.show_all()
     Gtk.main()
-
